7c7d0ef3d48777a47fe3a842b090686105af865a
Feng <feng@Fengs-MacBook-Pro.local>
2020-08-25 18:57:36 -0700

    question 1

--- practicum/week-1/question1.js
+++ practicum/week-1/question1.js
@@ -1,10 +1,21 @@
 /*
-Given an array of numbers, return the running sum of the array. 
+Given an array of numbers, return the running sum of the array.
 Running sum of a cell in the array is the sum of the cells up to that cell.
-For example given the input array [5, 6, 1], the running sum is 
+For example given the input array [5, 6, 1], the running sum is
 [5, 5+6, 5+6+1]
 */
 
 var runningSum = function(input) {
+   var count;
+   var sum = 0;
+   for (var i = 0; i < input.length; i++) {
+     count = i;
+     while (count > -1) {
+       sum += input[count];
+       count--;
+     }
+     input[i] = sum;
+   }
    
+   return input;
 };
