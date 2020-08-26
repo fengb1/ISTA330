a94193a70dfb463a6080e6df72b3ea0fbb0ad3a7
Feng <feng@Fengs-MacBook-Pro.local>
2020-08-25 22:20:34 -0700

    question 10

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -7,5 +7,11 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
+  var num = n.toString();
+  var arr = [];
+  for (var i = 0; i < num.length; i++) {
+    arr[i] = parseInt(num.charAt(i));
+  }
 
+  return Math.max(...arr) - Math.min(...arr);
 };
