f894a410d63a3401083b06a0db0a53b582e8a434
Feng <feng@Fengs-MacBook-Pro.local>
2020-08-25 21:30:05 -0700

    question 6

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -1,10 +1,16 @@
 /*
-Given an array of non-negative integers, return  the median (https://en.wikipedia.org/wiki/Median) of the array. 
-Example: 
+Given an array of non-negative integers, return  the median (https://en.wikipedia.org/wiki/Median) of the array.
+Example:
 input: [1, 2, 2, 3, 4, 7, 9]
-output: 3 
+output: 3
 */
 
 var median = function(input) {
-
+  var length = input.length;
+  if (length % 2 == 1) {
+    return input[length / 2 - 0.5];
+  }
+  else {
+    return (input[length / 2] + input[length / 2 - 1]) / 2;
+  }
 };
