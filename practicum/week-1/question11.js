fe631dddeccef80c854ca082c17c444be05b1c82
Feng <feng@Fengs-MacBook-Pro.local>
2020-08-25 22:26:42 -0700

    question 11

--- practicum/week-1/question11.js
+++ practicum/week-1/question11.js
@@ -1,6 +1,6 @@
 /*
 Given an array of integers, input, representing a list encoded, decode the list.
-Every pair (2*i, 2*i + 1) of elements in the input, 
+Every pair (2*i, 2*i + 1) of elements in the input,
 means in the decoded list we have as many as input[2*i]'s of the quantity  input[2*i + 1].
 
 Example:
@@ -9,5 +9,14 @@ output: [5, 5, 2, 2, 2, 2]
 */
 
 var decode = function(input) {
+  var result = [];
+  for (var i = 0; i < input.length; i++) {
+    if (i % 2 == 0) {
+      for (var j = 0; j < input[i]; j++) {
+        result.push(input[i + 1]);
+      }
+    }
+  }
 
+  return result;
 };
