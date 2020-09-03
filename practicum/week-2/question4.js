d67204f0c0e67ed772605d5b041b612398b9c669
Feng <feng@Fengs-MacBook-Pro.local>
2020-09-03 15:29:03 -0700

    question 4

--- practicum/week-2/question4.js
+++ practicum/week-2/question4.js
@@ -1,17 +1,26 @@
 /*
-Given an array of integers, 
+Given an array of integers,
 return true if the array contains any duplicates.
-Your function should return true 
-if any value appears at least twice 
-in the array, 
-and it should return false 
+Your function should return true
+if any value appears at least twice
+in the array,
+and it should return false
 if every element is distinct.
- 
+
 Example:
  input: [19,1,1,23,23,4,3,23,1,2]
  output: true
 */
 
 var hasDuplicates = function(input) {
-
+    var result = false;
+    for (var i = 0; i < input.length; i++) {
+      for (var j = i + 1; j < input.length; j++) {
+        if (input[i] == input[j]) {
+          result = true;
+          break;
+        }
+      }
+    }
+    return result;
 };
