55dc6697faf1933ede06c26abfa8a96d91546b42
Feng <feng@Fengs-MacBook-Pro.local>
2020-09-03 14:32:08 -0700

    question 2

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -1,6 +1,6 @@
 
 /*
-Given an integer array, input, 
+Given an integer array, input,
 find the subarray
  which has the largest sum and return its sum.
 
@@ -10,6 +10,16 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
-    
+     var result = [];
+     var temp = 0;
+     for (var i = 0; i < input.length; i++) {
+       result.push(input[i]);
+       temp = input[i];
+       for (var j = i + 1; j < input.length; j++) {
+         temp += input[j];
+         result.push(temp);
+       }
+     }
+
+     return Math.max(...result);
  };
