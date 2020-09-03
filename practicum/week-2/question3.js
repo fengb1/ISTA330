a6dbe82a53e1f99818e4750832466e7a7256ee50
Feng <feng@Fengs-MacBook-Pro.local>
2020-09-03 15:26:31 -0700

    question 3

--- practicum/week-2/question3.js
+++ practicum/week-2/question3.js
@@ -1,5 +1,5 @@
 /*
-Given a non-negative integer n, 
+Given a non-negative integer n,
 generate the first n rows of Pascal's triangle (https://en.wikipedia.org/wiki/Pascal%27s_triangle).
 
 Example:
@@ -8,10 +8,24 @@ output: [
      [1],
     [1,1],
    [1,2,1],
-  [1,3,3,1] 
+  [1,3,3,1]
 ]
 */
 
 var PascalTriangle = function(n) {
-
+    var result = new Array(n);
+    for (var k = 0; k < result.length; k++) {
+      result[k] = new Array(k + 1);
+    }
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < i + 1; j++) {
+        if (i == j || j == 0) {
+          result[i][j] = 1;
+        }
+        else {
+          result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+      }
+    }
+    return result;
 };
