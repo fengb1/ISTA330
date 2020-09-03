138d6a7a024cbab9311f3725e37ffdd0aff6a436
Feng <feng@Fengs-MacBook-Pro.local>
2020-09-03 15:49:49 -0700

    question 6

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -1,16 +1,28 @@
 /*
-Given an array of size n, 
-find the m-element. 
-The m-element is 
+Given an array of size n,
+find the m-element.
+The m-element is
 the element that appears more than ⌊ n/2 ⌋ times.
 
 If the majority element does not exist return undefined.
 
-Example: 
+Example:
 input: [1, 2, 2, 3, 2, 7, 2]
-output: 2 
+output: 2
 */
 
 var m_element = function(input) {
-
+    var times = input.length / 2;
+    for (var i = 0; i < input.length; i++) {
+      var count = 0
+      for (var j = 0; j < input.length; j++) {
+        if (input[i] == input[j]) {
+          count++;
+        }
+      }
+      if (count > times) {
+        return input[i];
+      }
+    }
+    return -1;
 };
