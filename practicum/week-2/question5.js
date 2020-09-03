e989272b0323d0bc32d714446a8b156ca1fd42c3
Feng <feng@Fengs-MacBook-Pro.local>
2020-09-03 15:36:00 -0700

    question 5

--- practicum/week-2/question5.js
+++ practicum/week-2/question5.js
@@ -1,23 +1,40 @@
 /*
-An array is monotonic if it is 
-either monotone increasing 
+An array is monotonic if it is
+either monotone increasing
 or monotone decreasing.
 
-An array A is monotone increasing 
-if for all i <= j, A[i] <= A[j].  
-An array A is monotone decreasing 
+An array A is monotone increasing
+if for all i <= j, A[i] <= A[j].
+An array A is monotone decreasing
 if for all i <= j, A[i] >= A[j].
 
-For a given array, input, 
+For a given array, input,
 return true if and only if the input array is monotonic.
 
-Example: 
+Example:
 input: [12, 6, 2, 2, 2, 0]
 output: true
 */
 
 var isMonotonic = function(input) {
-
+    var increasing = true;
+    var decreasing = true;
+    for (var i = 1; i < input.length - 1; i++) {
+      if (!(input[i - 1] <= input[i] && input[i] <= input[i + 1])) {
+        increasing = false;
+        break;
+      }
+    }
+    for (var i = 1; i < input.length - 1; i++) {
+      if (!(input[i - 1] >= input[i] && input[i] >= input[i + 1])) {
+        decreasing = false;
+        break;
+      }
+    }
+    if (increasing || decreasing) {
+      return true;
+    }
+    else {
+      return false;
+    }
 };
-
-console.log('hi');
