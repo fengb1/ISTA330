24a8622a4eb3643897b8584c19f4aff4be4568c8
Feng <feng@Fengs-MacBook-Pro.local>
2020-09-03 15:56:59 -0700

    question 8

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -1,9 +1,9 @@
 /*
-Given an array of integers, input, 
-a d-integer is an integer which has 
+Given an array of integers, input,
+a d-integer is an integer which has
 a frequency in the array equal to its value.
 
-Return a d-integer. If there are multiple 
+Return a d-integer. If there are multiple
 d-integers return the largest of them.
 If there is no d-integer return -1.
 
@@ -13,5 +13,17 @@ output: 3
 */
 
 var d_integer = function(input) {
-
+    for (var i = 0; i < input.length; i++) {
+      var count = 0;
+      for (var j = 0; j < input.length; j++) {
+        if (input[i] == input[j]) {
+          count++;
+        }
+      }
+      if (count == input[i]) {
+        return input[i];
+        break;
+      }
+    }
+    return -1;
 };
