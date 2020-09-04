85a2de2bbf18322c4db4eed272ecbb53fc43b8ff
Feng <feng@Fengs-MacBook-Pro.local>
2020-09-03 17:45:33 -0700

    question 12

--- practicum/week-2/question12.js
+++ practicum/week-2/question12.js
@@ -1,6 +1,6 @@
 /*
-The Fibonacci numbers, denoted as F(n) is a sequence such that 
-each number is the sum of the two preceding ones. 
+The Fibonacci numbers, denoted as F(n) is a sequence such that
+each number is the sum of the two preceding ones.
 That is:
 F(0) = 0,   F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1.
@@ -9,5 +9,13 @@ Given n, calculate F(n).
 */
 
 var F = function(n) {
-
+    var x = 1;
+    var y = 0;
+    var sum;
+    for (var i = 0; i < n; i++) {
+      sum = x + y;
+      x = y;
+      y = sum;
+    }
+    return y;
 };
