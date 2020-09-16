ce5775fabca6e700179e48efbc4bfcb93861cbf2
fengb1 <fengb1@email.arizona.edu>
2020-09-16 10:50:58 -0700

    question 12

--- practicum/week-3/question12.js
+++ practicum/week-3/question12.js
@@ -1,8 +1,8 @@
 /*
-Given a string path, where path[i] = 'N', 'S', 'E' or 'W', each representing moving one unit north, south, east, or west, respectively. 
+Given a string path, where path[i] = 'N', 'S', 'E' or 'W', each representing moving one unit north, south, east, or west, respectively.
 You start at the origin (0, 0) on a 2D plane and walk on the path specified by path.
 
-Return true if the path crosses itself at any point, that is, if at any time you are on a location you've previously visited. 
+Return true if the path crosses itself at any point, that is, if at any time you are on a location you've previously visited.
 Return false otherwise.
 
 Example:
@@ -11,5 +11,23 @@ Example:
 */
 
 var isCrossing = function(s) {
-
+  var record = [0, 0];
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] == "N") {
+      record[0] += 1;
+    }
+    else if (s[i] == "S") {
+      record[0] -= 1;
+    }
+    else if (s[i] == "E") {
+      record[1] += 1;
+    }
+    else if (s[i] == "W") {
+      record[1] -= 1;
+    }
+    if (record[0] == 0 && record[1] == 0) {
+      return true;
+    }
+  }
+  return false;
 };
