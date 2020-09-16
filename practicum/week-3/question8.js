393b40252f503880f71a946b1ffebf42ba63fb57
fengb1 <fengb1@email.arizona.edu>
2020-09-16 09:55:54 -0700

    question 8

--- practicum/week-3/question8.js
+++ practicum/week-3/question8.js
@@ -1,6 +1,6 @@
 /*
-Given an array with n objects colored red, white or blue, 
-sort them in-place(refer to https://en.wikipedia.org/wiki/In-place_algorithm) so that objects of the same color are adjacent, 
+Given an array with n objects colored red, white or blue,
+sort them in-place(refer to https://en.wikipedia.org/wiki/In-place_algorithm) so that objects of the same color are adjacent,
 with the colors in the order red, white and blue.
 
 We will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
@@ -13,5 +13,14 @@ output: [0,0,1,1,2,2]
 */
 
 var sortColors = function(input) {
-
+  for (var i = 0; i < input.length; i++) {
+    for (var j = 0; j < input.length - 1; j++) {
+      if (input[j] > input[j + 1]) {
+        var temp = input[j];
+        input[j] = input[j + 1];
+        input[j + 1] = temp;
+      }
+    }
+  }
+  return input;
 };
