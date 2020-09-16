9636e068421bbe982489e8f65d2f767453fe32d7
fengb1 <fengb1@email.arizona.edu>
2020-09-16 08:40:41 -0700

    question 2

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -1,16 +1,22 @@
 
 /*
-Given an array of integers, 1 ≤ input[i] ≤ n (n = size of array), 
+Given an array of integers, 1 ≤ input[i] ≤ n (n = size of array),
 some elements appear twice and others appear once.
 
 Find all the elements that appear twice in this array.
 
 Example: input: [19,3,2,10,8,2,3,5]
          output: [2,3]
-                
+
           */
 
  var findDuplicates = function(input) {
-     
-    
+     var arr = input.slice().sort();
+     var result = [];
+     for (var i = 0; i < arr.length - 1; i++) {
+       if (arr[i] == arr[i + 1]) {
+         result.push(arr[i]);
+       }
+     }
+     return result;
  };
