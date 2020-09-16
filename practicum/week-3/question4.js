aaf556ddb7b1822df36de47f65f59d9e5b2d995b
fengb1 <fengb1@email.arizona.edu>
2020-09-16 09:07:41 -0700

    question 4

--- practicum/week-3/question4.js
+++ practicum/week-3/question4.js
@@ -1,16 +1,16 @@
 /*
-Given a set of distinct integers, input, 
+Given a set of distinct integers, input,
 return all possible subsets (the power set).
 
 The solution set must not contain duplicate subsets.
- 
+
 Example:
  input: [1,2,3]
  output: [
-  [],   
+  [],
   [1],
   [2],
-  [3],  
+  [3],
   [1,3],
   [2,3],
   [1,2],
@@ -19,5 +19,17 @@ Example:
 */
 
 var powerSet = function(input) {
-
+  var subset = [];
+  var result = [];
+  for (var i = 0; i < Math.pow(2, input.length); i++) {
+    for (var j = 0; j < input.length; j++) {
+      console.log(i & (1<<j));
+      if ((i & (1<<j)) > 0) {
+        subset.push(input[j]);
+      }
+    }
+    result.push(subset);
+    subset = [];
+  }
+  return result;
 };
