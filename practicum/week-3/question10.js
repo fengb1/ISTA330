aec3058f86421a3d1b63866ae92da71606adbc92
fengb1 <fengb1@email.arizona.edu>
2020-09-16 10:25:21 -0700

    question 10(uncertain about the question mark)

--- practicum/week-3/question10.js
+++ practicum/week-3/question10.js
@@ -1,8 +1,8 @@
 /*
-Given a string, determine if it is a palindrome, 
+Given a string, determine if it is a palindrome,
 considering only alphanumeric characters and ignoring cases.
 
-Note: For the purpose of this question, 
+Note: For the purpose of this question,
 we define empty string as valid palindrome.
 
 Example:
@@ -11,5 +11,14 @@ output: true
 */
 
 var isPalindrome = function(s) {
-
+  var regex = /^[a-z0-9 ?]+$/i;
+  if (s.length == 0) {
+    return true;
+  }
+  else if (s.match(regex)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
