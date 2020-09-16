f4ea477f5d5501c19d55e5b88bf60e823273f728
fengb1 <fengb1@email.arizona.edu>
2020-09-16 10:10:56 -0700

    question 9

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -1,6 +1,6 @@
 /*
-Given an array of integers A, 
-a move consists of choosing any A[i], 
+Given an array of integers A,
+a move consists of choosing any A[i],
 and incrementing it by 1.
 
 Return the least number of moves to make every value in A unique.
@@ -11,5 +11,13 @@ output: 1
 */
 
 var minMoves = function(A) {
-
+  var arr = A.slice().sort();
+  var count = 0;
+  for (var i = 0; i < arr.length - 1; i++) {
+    while (arr[i + 1] <= arr[i]) {
+      arr[i + 1] += 1;
+      count++;
+    }
+  }
+  return count;
 };
